f9470e96d5e9898a8f59751e11212f72cebcab9d
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-02-12 16:13:34 +0000

    Treat video as 3D array of data and transpose it
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

---
+++ v3d.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# args: 
+#   1: input file name, defaults to input.mp4
+#   2: output file name, defaults to output.mp4
+#   3: max frames to transform, 0 means unlimited and is the default
+#   4: shape, default -> thw, wth
+
+from sys import argv
+import cv2
+import numpy as np
+
+# Load video
+video_path = "input.mp4"
+if len(argv) > 1:
+    video_path = argv[1]
+output_path = "output.mp4"
+if len(argv) > 2:
+    output_path = argv[2]
+print(f"reading {video_path}.")
+cap = cv2.VideoCapture(video_path)
+
+# Get video properties
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+if frame_count == 0 or frame_width == 0 or frame_height == 0 or fps == 0:
+    print(f"cannot decode {video_path}.")
+    exit(1)
+print(f"{video_path}: {frame_width}x{frame_height}, {fps} FPS, {frame_count} frames.")
+
+# Read frames into a NumPy array
+frames = []
+max_frames = 0
+if len(argv) > 3:
+    max_frames = int(argv[3])
+    print(f"processing no more than {max_frames}.")
+
+# shapes
+shape = 'thw'
+if len(argv) > 4:
+    shape = argv[4].lower()
+print(f"new shape {shape}.")
+
+n = 0
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frames.append(frame)
+    n += 1
+    if max_frames > 0 and n >= max_frames:
+        break
+cap.release()
+print(f"read {len(frames)} frames.")
+# Shape: (T, H, W, C)
+frames = np.array(frames)
+print(f"created NP array ({n} items).")
+
+if shape == 'thw':
+    # Swap time and width dimensions (T, H, W, C) → (W, H, T, C)
+    transformed = np.transpose(frames, (2, 1, 0, 3))
+elif shape == 'wth':
+    # Swap height and time dimensions (T, H, W, C) → (W, T, H, C)
+    transformed = np.transpose(frames, (2, 0, 1, 3))
+else:
+    print(f"unknown new shape {shape} just applying MJPEG re-encoding.")
+    transformed = np.transpose(frames, (0, 1, 2, 3))
+    print(f"applied transpose.")
+
+# Get new video size
+new_frame_count, new_height, new_width, _ = transformed.shape
+print(f"{output_path} will be: {new_width}x{new_height}, {fps} FPS, {new_frame_count} frames.")
+
+# Save the transformed video
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = cv2.VideoWriter(output_path, fourcc, fps, (new_width, new_height))
+
+print(f"writing {output_path} {new_frame_count} frames.")
+for i in range(new_frame_count):
+    if i > 0 and i % 100 == 0:
+        print(f"{i} frame.")
+    out.write(transformed[i])
+
+out.release()
+print(f"transformed video saved as {output_path}.")
